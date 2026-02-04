cc3ddae4f77272897ef0aa0a3a9524dda8c6a367
Natalia <natalia.marcos@jcyl.educa.es>
2026-02-04 16:46:00 +0100

    Ejercicio para aprender que se hace una clase para constantes para guardar datos.

--- src/UT04Clase04Cosa/AnimalPluricelularVertebrado.java
+++ src/UT04Clase04Cosa/AnimalPluricelularVertebrado.java
@@ -1,4 +1,8 @@
 package UT04Clase04Cosa;
 
 public class AnimalPluricelularVertebrado extends AnimalPluricelular {
+
+
+    int patas;
+
 }
---
+++ src/UT04Clase04Cosa/main.java
@@ -0,0 +1,12 @@
+package UT04Clase04Cosa;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        AnimalPluricelularVertebrado araña = new AnimalPluricelularVertebrado();
+        araña.patas = ConstantesBiologia.PATAS_ARAÑAS;
+
+    }
+
+}
