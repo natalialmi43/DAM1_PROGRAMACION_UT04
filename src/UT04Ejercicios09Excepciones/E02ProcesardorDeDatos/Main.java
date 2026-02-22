7a4705a7caeaf62006cbcfc230367107f08262a7
Natalia <natalia.marcos@educa.jcyl.es>
2026-02-22 14:16:46 +0100

    Finalizado, se usa Buffered, y lanza tres excecpciones direrentes

---
+++ src/UT04Ejercicios09Excepciones/E02ProcesardorDeDatos/Main.java
@@ -0,0 +1,11 @@
+package UT04Ejercicios09Excepciones.E02ProcesardorDeDatos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ProcesadorDatos p1 = new ProcesadorDatos();
+
+        p1.procesarEntrada();
+
+    }
+}
