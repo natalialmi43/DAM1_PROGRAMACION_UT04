90f2a205051e8aecc8ecc80c537d8eed39c7befe
Natalia <natalia.marcos@educa.jcyl.es>
2026-02-23 08:20:46 +0100

    WIP

---
+++ src/UT04Repaso/E02FlotaEspacial/Elementos.java
@@ -0,0 +1,43 @@
+package UT04Repaso.E02FlotaEspacial;
+
+public class Elementos {
+
+    private boolean transmision;
+    private String reporteEstado;
+    private double anchoDeBanda;
+
+    public Elementos(String reporteEstado, double anchoDeBanda) {
+        setTransmision(transmision);
+        setReporteEstado(reporteEstado);
+        setAnchoDeBanda(anchoDeBanda);
+    }
+
+    public void setTransmision(boolean transmision) {
+        this.transmision = true;
+    }
+
+    public void setReporteEstado(String reporteEstado) {
+        if (reporteEstado == null || reporteEstado.isEmpty()){
+            this.reporteEstado = "Defecto";
+        } else {
+            this.reporteEstado = reporteEstado;
+        }
+
+    }
+
+    public void setAnchoDeBanda(double anchoDeBanda) {
+        if (anchoDeBanda < 0){
+            this.anchoDeBanda = 0;
+        }else {
+            this.anchoDeBanda = anchoDeBanda;
+        }
+    }
+
+    public void intentoTrasnsmision (){
+
+        if (transmision && anchoDeBanda <= 0){
+            throw new IllegalArgumentException("Error: NO PUEDES TRANSMITIR");
+        }
+
+    }
+}
---
+++ src/UT04Repaso/E02FlotaEspacial/RoversDeSuperficie.java
@@ -0,0 +1,26 @@
+package UT04Repaso.E02FlotaEspacial;
+
+public class RoversDeSuperficie extends VehiculosMotorizados {
+
+    private int numeroRuedas;
+    private double compartimentoRocasKg;
+
+    public RoversDeSuperficie(String reporteEstado, double anchoDeBanda, String numeroSerie, double cantidadBateria, double velocidadMaxima, int numeroRuedas, double compartimentoRocasKg) {
+        super(reporteEstado, anchoDeBanda, numeroSerie, cantidadBateria, velocidadMaxima);
+        this.numeroRuedas = numeroRuedas;
+        this.compartimentoRocasKg = compartimentoRocasKg;
+    }
+
+    public void setNumeroRuedas(int numeroRuedas) {
+        this.numeroRuedas = numeroRuedas;
+    }
+
+    public void setCompartimentoRocasKg(double compartimentoRocasKg) {
+        this.compartimentoRocasKg = compartimentoRocasKg;
+    }
+
+    @Override
+    protected double consumoBateriaDispositivo(double kmRecorridos) {
+        return kmRecorridos * (2 + (0.5*compartimentoRocasKg));
+    }
+}
